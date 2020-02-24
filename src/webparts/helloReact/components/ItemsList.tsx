3fd99930c881e9d5a1260076e54ff6864f01e0eb
Thomas Leclaire <thomas@leclaire.de>
2020-02-24 13:37:04 +0100

    Listendarstellung verbessert

--- src/webparts/helloReact/components/ItemsList.tsx
+++ src/webparts/helloReact/components/ItemsList.tsx
@@ -1,39 +1,43 @@
-import * as React from 'react';
-import { IItem } from './IItem';
-import { List } from 'office-ui-fabric-react/lib/List';
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import * as React from "react";
+import { IItem } from "./IItem";
+import { List } from "office-ui-fabric-react/lib/List";
+import { DefaultButton } from "office-ui-fabric-react/lib/Button";
 
 export type EditItemCallback = (item: IItem) => void;
 
 export interface IItemsListProps {
   items: IItem[];
-  onEditItem : EditItemCallback;
+  onEditItem: EditItemCallback;
 }
-
 
 export class ItemsList extends React.Component<IItemsListProps, {}> {
   public render(): React.ReactElement<IItemsListProps> {
-      return (
-          <div>
-              <List items={this.props.items} onRenderCell={this._onRenderListCell} />
-          </div>
-      );
+    return (
+      <table>
+        <List items={this.props.items} onRenderCell={this._onRenderListCell} />
+      </table>
+    );
   }
 
-  private _onRenderListCell = (item: IItem, index: number | undefined): JSX.Element => {
+  private _onRenderListCell = (
+    item: IItem,
+    index: number | undefined
+  ): JSX.Element => {
     return (
-        <div>
-            {item.title}<br />
-            <DefaultButton text="Edit"
-                data={item.id}
-                onClick={() => this._onButtonClick(item)}
-            />
-        </div>
+      <tr>
+        <td>
+          <DefaultButton
+            text="Edit"
+            data={item.id}
+            onClick={() => this._onButtonClick(item)}
+          />
+        </td>
+        <td>{item.title}</td>
+      </tr>
     );
-}
+  }
 
-private _onButtonClick(item: IItem): void {
-  this.props.onEditItem(item);
-}
-
+  private _onButtonClick(item: IItem): void {
+    this.props.onEditItem(item);
+  }
 }
